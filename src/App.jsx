9650b5d260fc5dfbbc60af96a37f1d4ae4c35aa5
Nyoman Lanang Kresna Erlangga <48331946+dimnyan@users.noreply.github.com>
2024-05-27 00:53:22 +0700

    add link

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ function App() {
     <div className="container">
       <h1 className="title">Bleh</h1>
       <img src="/bleh.jpg" />
+      <p>Created By </p>
+      <a href="https://dimnyan.tech">Dimnyan</a>
     </div>
   );
 }
